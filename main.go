bbe2f4775d6ab0c75468d6305e92f489a47b7fdb
Bherly Novrandy <pushm0v.development@gmail.com>
2018-05-16 11:10:19 +0700

    [bherly] Add router config

--- main.go
+++ main.go
@@ -1,8 +1,27 @@
 package main
 
-import "github.com/BaritoLog/barito-router/router"
+import (
+	"github.com/BaritoLog/barito-router/router"
+	"os"
+)
+
+const (
+	EnvRouterAddress   = "BARITO_ROUTER_ADDRESS"
+	EnvBaritoMarketUrl = "BARITO_ROUTER_MARKET_URL"
+)
 
 func main() {
-	r := router.NewRouter(":8080")
+	routerAddress := os.Getenv(EnvRouterAddress)
+	if routerAddress == "" {
+		routerAddress = ":8080"
+	}
+
+	baritoMarketUrl := os.Getenv(EnvBaritoMarketUrl)
+	if baritoMarketUrl == "" {
+		baritoMarketUrl = "http://localhost:3000/apps"
+	}
+
+	t := router.NewTrader(baritoMarketUrl)
+	r := router.NewRouter(routerAddress, t)
 	r.Server().ListenAndServe()
 }
