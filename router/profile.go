61845987e7b5fa1132f4702b7bd5b86d1d5efa4f
Giovanni Sakti <giosakti@gmail.com>
2018-08-02 11:49:42 +0700

    [Gio/Iman] modify fetchProfileBySecret to use new contract

--- router/profile.go
+++ router/profile.go
@@ -5,17 +5,32 @@ import (
 )
 
 type Profile struct {
+	ID          string       `json:"id"`
 	Name        string       `json:"name"`
-	AppGroup    string       `json:"app_group"`
-	TpsConfig   string       `json:"tps_config"`
+	AppGroup    string       `json:"app_group_name"`
+	MaxTps      int          `json:"max_tps"`
 	ClusterName string       `json:"cluster_name"`
 	ConsulHost  string       `json:"consul_host"`
-	AppStatus   string       `json:"app_status"`
+	AppStatus   string       `json:"status"`
 	Meta        *ProfileMeta `json:"meta"`
 }
 
 type ProfileMeta struct {
-	ServiceNames map[string]string `json:"service_names"`
+	ServiceNames  map[string]string  `json:"service_names"`
+	Kafka         *KafkaMeta         `json:"kafka"`
+	Elasticsearch *ElasticsearchMeta `json:"elasticsearch"`
+}
+
+type KafkaMeta struct {
+	TopicName         string `json:"topic_name"`
+	Partition         string `json:"partition"`
+	ReplicationFactor string `json:"replication_factor"`
+	ConsumerGroup     string `json:"consumer_group"`
+}
+
+type ElasticsearchMeta struct {
+	IndexPrefix  string `json:"index_prefix"`
+	DocumentType string `json:"document_type"`
 }
 
 func NewProfileFromBytes(b []byte) (*Profile, error) {
