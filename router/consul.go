3b48a4e3d8d0d86e478c70938d0065570bab9058
bentol <bentol@users.noreply.github.com>
2019-12-18 13:18:03 +0700

    update consul to randomize service from multiple service available (#29)
    * update consul to randomize service from multiple service available
    
    * tidy up code on consul module
    
    * fix indentation on consul module

--- router/consul.go
+++ router/consul.go
@@ -5,47 +5,71 @@ import (
 	"github.com/BaritoLog/barito-router/config"
 	"github.com/hashicorp/consul/api"
 	"github.com/patrickmn/go-cache"
+	"math/rand"
+	"time"
 )
 
 const ConsulServiceBackupCachePrefix = "consul_backup_cache_"
 
-func consulService(consulAddr, serviceName string, cacheBag *cache.Cache) (srv *api.CatalogService, err error) {
-	return fetchProducerUsingCache(cacheBag, consulAddr+"_"+serviceName, func() (srv *api.CatalogService, err error) {
-		consulClient, _ := api.NewClient(&api.Config{
-			Address: consulAddr,
-		})
+type ConsulCatalog interface {
+	Service(service, tag string, q *api.QueryOptions) ([]*api.CatalogService, *api.QueryMeta, error)
+}
 
-		services, _, err := consulClient.Catalog().Service(serviceName, "", nil)
+func consulService(consulAddr, serviceName string, cacheBag *cache.Cache) (srv *api.CatalogService, err error) {
+	consulClient, _ := api.NewClient(&api.Config{
+		Address: consulAddr,
+	})
+	return fetchConsulService(consulClient.Catalog(), consulAddr, serviceName, cacheBag)
+}
+
+func fetchConsulService(consulCatalog ConsulCatalog, consulAddr string, serviceName string, cacheBag *cache.Cache) (srv *api.CatalogService, err error) {
+	cacheKey := "services_" + consulAddr + "_" + serviceName
+	services, err := fetchServicesUsingCache(cacheBag, cacheKey, func() (consulServices []*api.CatalogService, err error) {
+		consulServices, _, err = consulCatalog.Service(serviceName, "", nil)
 		if err != nil {
 			return
 		}
 
-		if len(services) < 1 {
+		if len(consulServices) < 1 {
 			err = fmt.Errorf("No consul service found for '%s'", serviceName)
 			return
 		}
 
-		srv = services[0]
 		return
 	})
-}
 
-func fetchProducerUsingCache(cacheBag *cache.Cache, key string, function func() (*api.CatalogService, error)) (srv *api.CatalogService, err error) {
-	// check if still in cache
-	if cacheValue, found := cacheBag.Get(key); found {
-		srv = cacheValue.(*api.CatalogService)
+	if err != nil {
 		return
 	}
-	srv, err = function()
 
-	if (err == nil) && (srv != nil) {
+	if len(services) < 1 {
+		err = fmt.Errorf("No consul service found for '%s'", serviceName)
+		return
+	}
+
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
+	randomInt := r.Intn(len(services))
+	srv = services[randomInt]
+	return
+}
+
+func fetchServicesUsingCache(cacheBag *cache.Cache, key string, function func() ([]*api.CatalogService, error)) (services []*api.CatalogService, err error) {
+	// check if still in cache
+	if cacheValue, found := cacheBag.Get(key); found {
+		services = cacheValue.([]*api.CatalogService)
+		return
+	}
+	services, err = function()
+
+	if (err == nil) && (services != nil) && (len(services) > 0) {
 		// push to cache
-		cacheBag.Set(key, srv, config.CacheExpirationTimeSeconds)
-		cacheBag.Set(ConsulServiceBackupCachePrefix+key, srv, config.BackupCacheExpirationTimeHours)
+		cacheBag.Set(key, services, config.CacheExpirationTimeSeconds)
+		cacheBag.Set(ConsulServiceBackupCachePrefix+key, services, config.BackupCacheExpirationTimeHours)
 	} else {
 		// if call is fail, check if still in backup cache
 		if cacheValue, found := cacheBag.Get(ConsulServiceBackupCachePrefix + key); found {
-			srv = cacheValue.(*api.CatalogService)
+			services = cacheValue.([]*api.CatalogService)
 		}
 	}
 
